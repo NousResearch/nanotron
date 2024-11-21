6d52c3f852ad7d448fb93dc651dde1e1d88eff92
NouamaneTazi <nouamane98@gmail.com>
2024-11-21 16:07:59 +0000

    move load custom func to base

--- src/nanotron/trainer.py
+++ src/nanotron/trainer.py
@@ -1,4 +1,5 @@
 import datetime
+import gc
 import json
 import os
 import shutil
@@ -438,8 +439,6 @@ class DistributedTrainer:
 
         prof = get_profiler(config=self.config)
         # free memory
-        import gc
-
         gc.collect()
         torch.cuda.empty_cache()
         with prof:
