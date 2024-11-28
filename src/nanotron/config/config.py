a72fd3428958c11d0796fb25bb58b22a03be599d
“eliebak” <elie.bakouch@huggingface.co>
2024-11-28 08:46:12 +0000

    style fix

--- src/nanotron/config/config.py
+++ src/nanotron/config/config.py
@@ -158,6 +158,8 @@ class CheckpointsArgs:
     save_initial_state: Optional[bool] = False
     save_final_state: Optional[bool] = False
     resume_checkpoint_path: Optional[Path] = None
+    load_lr_scheduler: Optional[bool] = True
+    load_optimizer: Optional[bool] = True
     checkpoints_path_is_shared_file_system: Optional[bool] = False
 
     def __post_init__(self):
